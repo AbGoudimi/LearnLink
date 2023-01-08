58221f629739745bfe6594c4bd36b72ac77f2730
AbGoudimi <ab.goudimi@gmail.com>
2023-01-08 15:09:24 +0100

    added home page with subject list

--- app/src/main/java/com/example/learnlink/HomeFragment.java
+++ app/src/main/java/com/example/learnlink/HomeFragment.java
@@ -2,11 +2,18 @@ package com.example.learnlink;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +30,8 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    List<Subject> subjects;
+    RecyclerView recyclerView;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -53,12 +61,37 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+        dataInitilize();
+        recyclerView = getView().findViewById(R.id.subjects_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+        SubjectAdapter adapter = new SubjectAdapter(getContext(),subjects);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void dataInitilize() {
+        subjects = new ArrayList<Subject>();
+        subjects.add(new Subject("c++",new Year("isi4")));
+        subjects.add(new Subject("c#",new Year("isi4")));
+        subjects.add(new Subject("java",new Year("isi4")));
+        subjects.add(new Subject("sgbd",new Year("isi4")));
     }
 }
